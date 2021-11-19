c2deba4a60bb96d474a6fe4632a2141fc0e47d4d
stringedcat <mauroox09@gmail.com>
2021-11-19 11:46:04 -0300

    completed challengue code

--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -3,6 +3,24 @@
 // The output is sorted in descending order by points
 // input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
 // output: [{ team: 'green', points: 40 }, ...]
-module.exports = (players) => {
-  return []
-}
+module.exports = (teams) => {
+  let arr = [];
+  const teamsActive = teams.filter((player) => player.isActive === true);
+  for (let p = 0; p < teamsActive.length; p++) {
+    if (!arr.find((element) => element.team === teamsActive[p].team)) {
+      arr.push({ team: teamsActive[p].team, points: 0 });
+    } else {
+      continue;
+    }
+  }
+
+  for (let i = 0; i < teamsActive.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (teamsActive[i].team === arr[j].team) {
+        arr[j].points += teamsActive[i].points;
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+};
