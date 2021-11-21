e9fcf4bd88bc9e28e8a2ced786ce5c8d8a989ec1
Mauro Loyola <mauroox09@gmail.com>
2021-11-21 07:34:35 -0300

    finish

--- src/__tests__/calculateTeamPoints.test.js
+++ src/__tests__/calculateTeamPoints.test.js
@@ -1,2 +1,12 @@
+const calculateTeam = require("../calculateTeamPoints");
+const teams = require("./__data__/players.json")
 describe('calculateTeamPoints', () => {
+test(" Teams", () => {
+    expect(calculateTeam(teams).toBe([
+        { team: 'red', points: 20 },
+        { team: 'blue', points: 10 },
+        { team: 'green', points: 40 }
+    ]))
+})
+
 })
